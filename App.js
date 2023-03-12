0bd6faf890c54f2b85e172996b7422c5b31ff345
Emily-Couvillon <125151996+Emily-Couvillon@users.noreply.github.com>
2023-03-12 16:10:05 -0400

    React homepage files

---
+++ App.js
@@ -0,0 +1,24 @@
+
+function App() {
+  return (
+    <div className="App">
+        <header>
+          <h1>Hike-athon</h1>
+          <div class="btn-right">
+              <button>Sign in</button>
+          </div>
+        </header>
+        <nav>
+            <a href="">Placeholder Text</a>
+            <a href="">Placeholder Text</a>
+            <a href="">Placeholder Text</a>
+        </nav><div class="map">
+            <img src="https://www.discoverdaytonohio.com/wp-content/uploads/place_lakes_and_parks_hueston_woods_20-1.jpg" alt="Map Image" />
+        </div><footer>
+            <p>Copyright HuestonGeeks 2023</p>
+        </footer>
+      </div>
+  );
+}
+
+export default App;
