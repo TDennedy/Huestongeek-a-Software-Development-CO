8024c961e5e6f6bb31c1b1dc56138aa04896b9f1
Emily-Couvillon <couvilek@DESKTOP-476GPBO.localdomain>
2023-04-10 23:16:06 -0400

    Updated both homepage files to match up with dashboard

--- dashboard/src/components/HomePage.jsx
+++ dashboard/src/components/HomePage.jsx
@@ -1,9 +1,13 @@
 import './homepage.css'
 import map from '../data/HWMap.png'
+import { SiRedwoodjs } from 'react-icons/si';
 
 export default function HomePage({ keycloak }) {
     return (
         <div>
+            <header className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+              <SiRedwoodjs /> <span>Hueston Geek</span>
+            </header>
             <nav>
                 <a href="">Placeholder Text</a>
                 <a href="">Placeholder Text</a>
